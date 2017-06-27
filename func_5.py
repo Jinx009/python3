21c869c93eb1459f4248b1eb9ece4543ba45dade
Jinx <jinxlovejinx@vip.qq.com>
2017-06-27 16:48:49 +0800

    函数

---
+++ func_5.py
@@ -0,0 +1,9 @@
+# 可写函数说明
+def print_info(name, age=35):
+    print('Name: ', name)
+    print('Age: ', age)
+    return
+
+
+print_info(age=50, name="jinx");
+print_info(name="jinx");
---
+++ func_6.py
@@ -0,0 +1,10 @@
+# 可写函数说明
+def total(arg1, arg2):
+    num = arg1 + arg2
+    print('函数内：', num)
+    return num
+
+
+num2 = total(10, 20)
+
+print('num:', num2)
