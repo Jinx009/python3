e102c8d8518a4fc0179d4cbbba8bec66c45c846e
Jinx <jinxlovejinx@vip.qq.com>
2017-06-28 09:43:35 +0800

    正则表达式

---
+++ re_1.py
@@ -0,0 +1,3 @@
+import re
+print(re.match('www', 'www.runoob.com').span())  # 在起始位置匹配
+print(re.match('com', 'www.runoob.com'))         # 不在起始位置匹配
---
+++ re_5.py
@@ -0,0 +1,11 @@
+import re
+
+
+# 将匹配的数字乘于 2
+def double(matched):
+    value = int(matched.group('value'))
+    return str(value * 2)
+
+
+s = 'A23G4HFD567'
+print(re.sub('(?P<value>\d+)', double, s))
